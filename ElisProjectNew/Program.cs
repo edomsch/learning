fde2ec962c8e22844bb4d00c3f4ab84df5a4dca5
elidomsch@icloud.com <elidomsch@icloud.com>
2019-09-17 19:22:17 -0500

    Test 2

---
+++ ElisProjectNew/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace ElisProjectNew
+{
+
+
+    public class Program
+    {
+        static int maxNum = 10;
+
+        public static void Main()
+        {
+            Console.WriteLine("Hi Eli!");
+
+            Console.WriteLine("Please enter the first number:");
+            var num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            var num2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Add(num1, num2));
+
+
+            var input = Console.ReadLine();
+        }
+
+
+
+        public Program()
+        {
+            Console.WriteLine("Hi Eli!");
+            //Console.WriteLine(input);
+        }
+    }
+
+    public static class Math
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
